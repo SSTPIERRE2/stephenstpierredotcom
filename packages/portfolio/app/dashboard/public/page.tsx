538722fc5c87d5c43495c78d84458ab2c94beedc
Steve St.Pierre <stephencstpierre@gmail.com>
2023-07-05 16:10:03 -0400

    upgrade all packages except @tsconfig/node16, add Plot package with virtual DOM implementation, public dashboard query DB directly from RSC WIP

--- packages/portfolio/app/dashboard/public/page.tsx
+++ packages/portfolio/app/dashboard/public/page.tsx
@@ -1,5 +1,26 @@
+import PlotFigure from '@/components/PlotFigure';
 import styles from './page.module.css';
+import { AnalyticsEvent } from '@core/analyticsEvent';
 
-export default function PublicDashboard() {
+// async function getMyEvents(visitorId?: string) {
+//   const res = visitorId
+//     ? await list([{ name: 'visitor_id', value: visitorId }])
+//     : [];
+//   return res;
+// }
+
+// async function getAllEvents() {
+//   const res = await AnalyticsEvent.list();
+//   return res;
+// }
+
+export default async function PublicDashboard({
+  searchParams,
+}: {
+  searchParams: { visitor?: string };
+}) {
+  // const myEvents = await getMyEvents(searchParams.visitor);
+  // const allEvents = await getAllEvents();
+
   return <main className={styles.main}>This is the Public Dashboard</main>;
 }
