9bb930924862e9b3df452c6cc07f143b2f22b6e4
Steve St.Pierre <stephencstpierre@gmail.com>
2024-03-11 13:17:48 -0400

    build WIP

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -1,45 +1,45 @@
-import {
-  use,
-  StackContext,
-  Api as ApiGateway,
-  Config,
-  // Auth,
-} from 'sst/constructs';
-import { Database } from './Database';
+// import {
+//   use,
+//   StackContext,
+//   Api as ApiGateway,
+//   Config,
+//   // Auth,
+// } from 'sst/constructs';
+// import { Database } from './Database';
 
-export function Api({ stack }: StackContext) {
-  const RESEND_API_KEY = new Config.Secret(stack, 'RESEND_API_KEY');
+// export function Api({ stack }: StackContext) {
+//   const RESEND_API_KEY = new Config.Secret(stack, 'RESEND_API_KEY');
 
-  // const auth = new Auth(stack, 'auth', {
-  //   authenticator: {
-  //     handler: 'packages/functions/src/auth.handler',
-  //   },
-  // });
+//   // const auth = new Auth(stack, 'auth', {
+//   //   authenticator: {
+//   //     handler: 'packages/functions/src/auth.handler',
+//   //   },
+//   // });
 
-  const api = new ApiGateway(stack, 'api', {
-    defaults: {
-      function: {
-        bind: [use(Database), RESEND_API_KEY],
-      },
-    },
-    routes: {
-      'POST /graphql': {
-        type: 'graphql',
-        function: {
-          handler: 'packages/functions/src/graphql/graphql.handler',
-        },
-      },
-    },
-    cors: true,
-  });
+//   const api = new ApiGateway(stack, 'api', {
+//     defaults: {
+//       function: {
+//         bind: [use(Database), RESEND_API_KEY],
+//       },
+//     },
+//     routes: {
+//       'POST /graphql': {
+//         type: 'graphql',
+//         function: {
+//           handler: 'packages/functions/src/graphql/graphql.handler',
+//         },
+//       },
+//     },
+//     cors: true,
+//   });
 
-  // auth.attach(stack, {
-  //   api,
-  // });
+//   // auth.attach(stack, {
+//   //   api,
+//   // });
 
-  stack.addOutputs({
-    API: api.url,
-  });
+//   stack.addOutputs({
+//     API: api.url,
+//   });
 
-  return api;
-}
+//   return api;
+// }
