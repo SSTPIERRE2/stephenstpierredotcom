f676fd30607c2b0f6cb30965c1103bb8a6a41185
Steve St.Pierre <stephencstpierre@gmail.com>
2024-03-15 12:26:10 -0400

    see if we need images remotePatterns

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { hostname: 'd10fsby4aele0n.cloudfront.net', protocol: 'https' },
-    ],
-  },
 };
 
 module.exports = nextConfig;
