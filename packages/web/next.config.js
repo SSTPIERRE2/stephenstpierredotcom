50e67b22251f728058fadf67f81ebd54f77de5c6
Steve St.Pierre <stephencstpierre@gmail.com>
2024-03-15 11:03:55 -0400

    fix next/babel eslint issue, add nextjs images config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { hostname: 'd10fsby4aele0n.cloudfront.net/', protocol: 'https' },
+    ],
+  },
 };
 
 module.exports = nextConfig;
