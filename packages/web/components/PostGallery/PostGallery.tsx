042d592c460f37a775caec50937eb950bd32ebec
Steve St.Pierre <stephencstpierre@gmail.com>
2024-03-28 13:20:35 -0400

    WIP

--- packages/web/components/PostGallery/PostGallery.tsx
+++ packages/web/components/PostGallery/PostGallery.tsx
@@ -5,24 +5,36 @@ import { PublishedPostWithTags } from '@core/post';
 import PostCard from '@/components/PostCard';
 import { useEffect, useState } from 'react';
 import PostSkeletonGallery from '../PostSkeletonGallery';
+// import useAbortController from '@/hooks/useAbortController';
 
 interface Props {
   getPosts: Function;
 }
 
 const PostGallery = ({ getPosts }: Props) => {
+  // const signal = useAbortController();
   const [posts, setPosts] = useState<PublishedPostWithTags[]>([]);
 
   useEffect(() => {
     const getData = async () => {
+      // return new Promise((resolve) => {
+      //   setTimeout(async () => {
+      console.log(`PostGallery getting posts...`);
       const data = await getPosts();
-      setPosts(data);
+      console.log(`PostGallery got posts`, data);
+
+      if (!!data && data.length) {
+        setPosts(data);
+      }
+      //     resolve();
+      //   }, 5000);
+      // });
     };
 
     getData();
   }, []);
 
-  if (posts.length) {
+  if (!!posts && posts.length) {
     return (
       <div className={styles.gallery}>
         {posts.map((post) => {
