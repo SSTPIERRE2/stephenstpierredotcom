b3e024f9a0702062e04a23fe7c811eb9a4f8561c
Steve St.Pierre <stephencstpierre@gmail.com>
2025-09-13 15:16:31 -0400

    fix nextjs config

--- infra/Web.ts
+++ infra/Web.ts
@@ -1,11 +1,10 @@
-import path from 'path';
 import { PostTable, TagTable } from './Database';
 
 const LOGROCKET_APP_ID = new sst.Secret('LOGROCKET_APP_ID');
 
 export const Web = new sst.aws.Nextjs('Web', {
   domain: $app.stage === 'prod' ? 'stephenstpierre.com' : undefined,
-  path: path.resolve('packages/web'),
+  path: 'packages/web',
   warm: $app.stage === 'prod' ? 20 : undefined,
   link: [PostTable, TagTable, LOGROCKET_APP_ID],
 });
--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: __dirname,
-  output: 'standalone',
 };
 
 module.exports = nextConfig;
